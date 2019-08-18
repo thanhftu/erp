a715c28356aaae928be1e0b78e49762e83566ef3
Thanh Truong <tcthanh.ftu@gmail.com>
2019-08-18 12:54:29 +0700

    heroku

--- erp/settings.py
+++ erp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -172,7 +173,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Login Settings
 # https://docs.djangoproject.com/en/1.8/topics/auth/
 from django.urls import reverse_lazy
